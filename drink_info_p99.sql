ba6a0b70e77c153bbf4739eb285d2e72ca7a1c4b
tadayouu <85947844+tadayouu@users.noreply.github.com>
2023-06-09 16:24:47 +0900

    2023-06-09
    おおもとのdrink_info.sqlのほうでNOT NULL制約を外したので修正した。

--- drink_info_p99.sql
+++ drink_info_p99.sql
@@ -1,8 +1,4 @@
 USE drinks;
 
-ALTER TABLE drink_info MODIFY COLUMN cost DEC(2,1);
-ALTER TABLE drink_info MODIFY COLUMN color VARCHAR(10);
-ALTER TABLE drink_info MODIFY COLUMN calories INT;
-
 INSERT INTO drink_info VALUES('ホリデー',NULL,14,NULL,'Y',50);
 INSERT INTO drink_info VALUES('ドラゴンブレス',2.9,7.2,'茶','N',NULL);
