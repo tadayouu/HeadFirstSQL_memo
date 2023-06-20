5e966f0fd4e66ba00c57e9b612e48e1c57943d5d
tadayouu <85947844+tadayouu@users.noreply.github.com>
2023-06-20 16:33:05 +0900

    black_book.sql

---
+++ black_book.sql
@@ -0,0 +1,16 @@
+USE gregs_list;
+CREATE TABLE black_book
+(
+date_name VARCHAR(30) NOT NULL,
+rating VARCHAR(30) NOT NULL
+);
+INSERT INTO black_book VALUES('アレックス','斬新');
+INSERT INTO black_book VALUES('ジェームス','退屈');
+INSERT INTO black_book VALUES('イアン','驚くべき');
+INSERT INTO black_book VALUES('ボリス','つまらない');
+INSERT INTO black_book VALUES('メルビン','下層階級');
+INSERT INTO black_book VALUES('エリック','情けない');
+INSERT INTO black_book VALUES('アンソニー','楽しい');
+INSERT INTO black_book VALUES('サミー','なかなかいい');
+INSERT INTO black_book VALUES('イヴァン','陰気');
+INSERT INTO black_book VALUES('ヴィック','話にならない');
